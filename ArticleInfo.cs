fb52c1a0f93c655fe09df9ee972011bcd4586106
feeleen <feeleen.ru@gmail.com>
2015-12-09 19:41:42 +0300

    Add        ArticleInfo.cs forgot to add :)

---
+++ ArticleInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Сводное описание для ArticleInfo
+/// </summary>
+public class ArticleInfo
+{
+	public long Id = 0;
+	public long Quantity = 0;
+
+	public ArticleInfo(long goodID, decimal quantity)
+	{
+		this.Id = goodID;
+		this.Quantity = Convert.ToInt64(quantity);
+	}
+}
