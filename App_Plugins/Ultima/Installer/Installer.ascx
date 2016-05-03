972563c5ea704cdc6ba45abf7cb88dc63e687bce
feeleen <feeleen.ru@gmail.com>
2016-05-03 12:10:52 +0300

    moved to app_code & added backoffice

---
+++ App_Plugins/Ultima/Installer/Installer.ascx
@@ -0,0 +1,12 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="Installer.ascx.cs" Inherits="App_Plugins_Ultima_Installer_Installer" %>
+
+<asp:Panel runat="server" ID="InstallerInfoPanel">
+
+	<asp:Label runat="server" ID="InstallerLabel"></asp:Label><br/>
+	
+	<asp:Button runat="server" ID="CompleteInstallationButton" OnClick="CompleteInstallationButton_Click" Text="Installation: Step 2"></asp:Button><br/>
+
+	<asp:Label runat="server" ID="InstallerLabelResult"></asp:Label>
+	
+
+</asp:Panel>
---
+++ App_Plugins/Ultima/Installer/Installer.ascx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Ultima;
+using umbraco;
+using Umbraco.Core;
+using Umbraco.Core.Models;
+
+public partial class App_Plugins_Ultima_Installer_Installer : System.Web.UI.UserControl
+{
+	protected void Page_Load(object sender, EventArgs e)
+	{
+		InstallerLabel.Text += "One Step left to get it done, please press the BUTTON ;)";	
+	}
+
+	protected void CompleteInstallationButton_Click(object sender, EventArgs e)
+	{
+		InstallHelpers.PublishContentNodes();
+		InstallerLabelResult.Text += " + Content Nodes has been published<br/>";
+		InstallHelpers.AddAppConfigSections();
+		InstallerLabelResult.Text += " + App.config has been modified<br/>";
+		InstallerLabelResult.Text += "<br/><b>Congratulations!</b> <br/> Please check http://ultima2c.com/ for more information!";
+
+	}
+
+}
