d76ef19471bc4dcba65f20661bb7f49597523fdb
feeleen <feeleen.ru@gmail.com>
2016-08-04 15:58:47 +0300

    Modified   App_Plugins/Ultima/backoffice/UltimaTree/partials/dashboard.html Add        App_Code/AppPool.cs added controller for appPool restarting

---
+++ App_Code/AppPool.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Http;
+using System.Xml;
+using Umbraco.Web.WebApi;
+
+/// <summary>
+/// Сводное описание для AppPool
+/// </summary>
+public class AppPoolController : UmbracoAuthorizedApiController
+{
+	const string appPoolRestartNodeName = "Ultima_LastAppPoolRestartTime";
+
+	[HttpGet]
+	public object Restart()
+	{
+		try
+		{
+			string time = DateTime.Now.ToString();
+
+			var webConfig = WebConfigurationManager.OpenWebConfiguration("/");
+			if (webConfig.AppSettings.Settings[appPoolRestartNodeName] != null)
+			{
+				webConfig.AppSettings.Settings[appPoolRestartNodeName].Value = time;
+			}
+			else
+			{
+				webConfig.AppSettings.Settings.Add(appPoolRestartNodeName, time);
+			}
+
+			webConfig.Save();
+
+			return true;
+		}
+		catch (Exception ex)
+		{
+		}
+
+		return false;
+	}
+}
