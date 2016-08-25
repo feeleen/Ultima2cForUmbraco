af36595481151498fa868cfd8e8077a35f673fe3
feeleen <feeleen.ru@gmail.com>
2016-08-25 12:39:03 +0300

    Modified   App_Code/SessionClient.cs Modified   Views/Partials/Login.cshtml optimization

--- App_Code/SessionClient.cs
+++ App_Code/SessionClient.cs
@@ -13,22 +13,66 @@ public class SessionClient
 
 	public static CClientInfo GetClientInfo()
 	{
+		return GetClientInfo(false);
+	}
+
+	public static bool SignIn(string user, string password)
+	{
+		bool isSigned = UltimaWebService.SignInAgent(user, password);
+		
+		if (isSigned)
+		{
+			CClientInfo info = SessionClient.GetClientInfo(true);
+
+			if (info != null)
+			{
+				bool isPersistent = false;
+				string userData = info.Id.ToString();
+
+				FormsAuthenticationTicket ticket = new FormsAuthenticationTicket(
+					1,                                     // ticket version
+					info.FirstName,                              // authenticated username
+					DateTime.Now,                          // issueDate
+					DateTime.Now.AddMinutes(30),           // expiryDate
+					isPersistent,                          // true to persist across browser sessions
+					userData,                              // can be used to store additional user data
+					FormsAuthentication.FormsCookiePath);  // the path for the cookie
+
+				string encryptedTicket = FormsAuthentication.Encrypt(ticket);
+
+				HttpCookie cookie = new HttpCookie(FormsAuthentication.FormsCookieName, encryptedTicket);
+				cookie.HttpOnly = true;
+				HttpContext.Current.Response.Cookies.Add(cookie);
+				return true;
+			}
+		}
+		else
+		{
+			SignOut();
+		}
+
+		return false;
+	}
+
+	public static CClientInfo GetClientInfo(bool askWebService)
+	{
 		CClientInfo clientInfo = (CClientInfo)HttpContext.Current.Session["ClientInfo"];
 		if (clientInfo == null)
 		{
-			try
+			if (askWebService)
 			{
-				//clientInfo = UltimaWebService.GetClientInfo();
-				//SetClientInfo(clientInfo);
-				return new CClientInfo();
+				try
+				{
+					clientInfo = UltimaWebService.GetClientInfo();
+					SetClientInfo(clientInfo);
+					return clientInfo;
+				}
+				catch (Exception ex)
+				{
+					SessionErrors.Add(ex.Message);
+					SignOut();
+				}
 			}
-			catch (Exception ex)
-			{
-				FormsAuthentication.SignOut();
-				HttpContext.Current.Response.Redirect("/");
-			}
-
-			return new CClientInfo();
 		}
 
 		return clientInfo;
@@ -37,5 +81,16 @@ public class SessionClient
 	public static void SetClientInfo(CClientInfo clientInfo)
 	{
 		HttpContext.Current.Session["ClientInfo"] = clientInfo;
+	}
+
+	public static void Clear()
+	{
+		HttpContext.Current.Session["ClientInfo"] = null;
+	}
+
+	public static void SignOut()
+	{
+		FormsAuthentication.SignOut();
+		Clear();
 	}
 }
