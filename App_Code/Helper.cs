840ca368736b63e01c980395b53094a8eb0388ff
feeleen <feeleen.ru@gmail.com>
2016-10-07 22:15:08 +0300

    Modified   Views/Goods.cshtml Modified   Views/Partials/CatalogFilter.cshtml Modified   Views/Partials/GoodGrid.cshtml Modified   assets/css/site.css Add        App_Code/Helper.cs fixed url parameters loss when using the filters

---
+++ App_Code/Helper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Сводное описание для Helper
+/// </summary>
+public class Helper
+{
+	private Helper()
+	{	
+	}
+
+
+	public static string GetQueryString(string parameter, string value, bool includeUrl = false)
+	{
+		var nameValues = HttpUtility.ParseQueryString(HttpContext.Current.Request.QueryString.ToString());
+		nameValues[parameter] = value;
+		string url = HttpContext.Current.Request.Url.AbsolutePath;
+		if (includeUrl)
+			return url + "?" + nameValues.ToString();
+		else
+			return nameValues.ToString();
+	}
+}
